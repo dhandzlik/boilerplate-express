a10350fabd64f3ba884d5317a4fcf2e6dd508fa3
Ezra Handzlik <dhandzlik@gmail.com>
2024-05-25 15:08:24 +0000

    uppercase

--- myApp.js
+++ myApp.js
@@ -1,39 +1,25 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use("/public", express.static(__dirname + "/public"));
 
+let response
 
+app.get("/json", (req, res) => {
+    if(process.env.MESSAGE_STYLE === "uppercase") {
+        response = "HELLO JSON"
+    } else {
+        response = "Hello json";
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    res.json({
+        message: response
+    })
+});
 
  module.exports = app;
